4f4a595ed30a6eace67cb3f30dd43af615fc41e7
Yury Lunev <yury.lunev@me.com>
2018-08-08 01:04:24 +0300

    Вставить контент с помощью javascript. Клонировать контент шаблона. Отрефакторить order на screen. Закешировать поиск стрелок по DOM.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 'use strict';
 const order = [`intro`, `greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`];
 const screens = order.map((screenName) => document.querySelector(`template#${screenName}`));
+const rootElement = document.querySelector(`#main`);
+const arrowButtons = document.querySelectorAll(`button.arrows__btn`);
+const arrowsWrapper = document.createElement(`div`);
 const showScreen = (screenNumber) => {
-  const rootElement = document.querySelector(`#main`);
-  rootElement.innerHTML = screens[screenNumber].innerHTML;
+  rootElement.innerHTML = ``;
+  rootElement.appendChild(document.importNode(screens[screenNumber].content, true));
 };
 
 let currentScreen = 0;
@@ -12,7 +15,7 @@ showScreen(currentScreen);
 document.addEventListener(`keyup`, (event) => {
   switch (event.key) {
     case `ArrowRight`:
-      currentScreen = Math.min(++currentScreen, order.length - 1);
+      currentScreen = Math.min(++currentScreen, screens.length - 1);
       break;
     case `ArrowLeft`:
       currentScreen = Math.max(--currentScreen, 0);
@@ -21,11 +24,30 @@ document.addEventListener(`keyup`, (event) => {
   showScreen(currentScreen);
 });
 
-document.querySelectorAll(`button.arrows__btn`)[0].addEventListener(`click`, () => {
+arrowsWrapper.className = `arrows__wrap`;
+arrowsWrapper.innerHTML = `<style>
+    .arrows__wrap {
+      position: absolute;
+      top: 95px;
+      left: 50%;
+      margin-left: -56px;
+    }
+
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn"><-</button>
+  <button class="arrows__btn">-></button>`;
+document.body.appendChild(arrowsWrapper);
+
+arrowButtons[0].addEventListener(`click`, () => {
   currentScreen = Math.max(--currentScreen, 0);
   showScreen(currentScreen);
 });
-document.querySelectorAll(`button.arrows__btn`)[1].addEventListener(`click`, () => {
-  currentScreen = Math.min(++currentScreen, order.length - 1);
+arrowButtons[1].addEventListener(`click`, () => {
+  currentScreen = Math.min(++currentScreen, screens.length - 1);
   showScreen(currentScreen);
 });
