a166fdd1560148ea15ab7c351001babbde381643
Yury Lunev <yury.lunev@me.com>
2018-08-26 20:24:14 +0300

    Исправить настройки проекта: gulpfile.js и package.json

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,26 @@ const mocha = require(`gulp-mocha`);
 const commonjs = require(`rollup-plugin-commonjs`);
 
 gulp.task(`style`, () => {
-  return gulp.src(`sass/style.scss`).pipe(plumber()).pipe(sass()).pipe(postcss([
-    autoprefixer({
-      browsers: [
-        `last 1 version`,
-        `last 2 Chrome versions`,
-        `last 2 Firefox versions`,
-        `last 2 Opera versions`,
-        `last 2 Edge versions`
-      ]
-    }),
-    mqpacker({sort: true})
-  ])).pipe(gulp.dest(`build/css`)).pipe(server.stream()).pipe(minify()).pipe(rename(`style.min.css`)).pipe(gulp.dest(`build/css`));
+  return gulp.src(`sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          `last 1 version`,
+          `last 2 Chrome versions`,
+          `last 2 Firefox versions`,
+          `last 2 Opera versions`,
+          `last 2 Edge versions`
+        ]
+      }),
+      mqpacker({sort: true})
+    ]))
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`sprite`, () => {
