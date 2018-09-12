499ad78d85e4fb85af0d422eca543fab7acefed7
Yury Lunev <yury.lunev@me.com>
2018-09-12 23:17:49 +0300

    Убрать страшный массив с экранами, но пока ещё не работает

--- js/game-content.js
+++ js/game-content.js
@@ -1,22 +1,7 @@
 import game1 from "./screen-game-1";
 import game2 from "./screen-game-2";
 import game3 from "./screen-game-3";
-import stats from "./screen-stats";
-import {isDead} from "./data/game-lives";
 
-const getGameContent = (state, callback1, callback2) => {
-  if ((state.level === state.answers.length) || isDead(state)) {
-    return stats(state, callback1, callback2);
-  }
-  switch (state.questions[state.level].length) {
-    case 2:
-      return game1(state, callback1, callback2);
-    case 1:
-      return game2(state, callback1, callback2);
-    case 3:
-      return game3(state, callback1, callback2);
-  }
-  return 0;
-};
+const getGameContent = (state) => [game2, game1, game3][state.questions[state.level].length - 1];
 
 export default getGameContent;
--- js/main.js
+++ js/main.js
@@ -3,96 +3,18 @@ import intro from "./screen-intro";
 import greeting from "./screen-greeting";
 import rules from "./screen-rules";
 import game1 from "./screen-game-1";
-import game2 from "./screen-game-2";
-import game3 from "./screen-game-3";
-import stats from "./screen-stats";
 import {getQuestions} from "./data/game-questions";
 import {INITIAL_GAME} from "./data/game-data";
-import {isFinalQuestion, nextLevel} from "./data/game-levels";
-import {addAnswer} from "./data/game-score";
-import {decreaseLives} from "./data/game-lives";
-
+import game2 from "./screen-game-2";
+import game3 from "./screen-game-3";
 
 const state = getQuestions(INITIAL_GAME);
 
-const SCREENS = [
-  {
-    name: intro,
-    callback: () => {
-      nextScreen(0);
-    }
-  },
-  {
-    name: greeting,
-    callback: () => {
-      nextScreen(1);
-    }
-  },
-  {
-    name: rules,
-    callback: () => {
-      nextScreen(2);
-    }
-  },
-  {
-    name: game2,
-    callback: () => {
-      console.log(`game2`);
-      nextScreen(3);
-    }
-  },
-  {
-    name: game1,
-    callback: () => {
-      console.log(`game1`);
-      const checkedAnswers = [...answers.filter((answer) => answer.control.checked)];
-      console.log(`Answers: ${checkedAnswers}`);
-      // if (checkedAnswers.length === 2) {
-      //   if (checkedAnswers.reduce((flag, answer, index) => (flag && (answer.control.value === state.questions[state.level][index][1])), true)) {
-      //     nextLevel(addAnswer(state, `correct`));
-      nextScreen(4);
-      //   } else {
-      //     nextLevel(addAnswer(decreaseLives(state), `wrong`));
-      //     nextScreen(4);
-      //   }
-      // }
-    }
-  },
-  {
-    name: game3,
-    callback: () => {
-      console.log(`game3`);
-      nextLevel(addAnswer(state, `correct`));
-      nextScreen(4);
-      // showScreen(getGameContent(nextLevel(addAnswer(state, `correct`))));
-      // showScreen(getGameContent(nextLevel(addAnswer(decreaseLives(state), `wrong`))));
-    }
-  },
-  {
-    name: stats,
-    callback: () => {
-    }
-  }];
-
-const getNextScreenNumber = (currentScreen) => {
-  if (currentScreen < 3) {
-    return currentScreen + 1;
-  } else {
-    if (isFinalQuestion(state)) {
-      return 6;
-    } else {
-      return state.questions[state.level].length + 3;
-    }
-  }
+const showNextScreen = (screen) => {
+  const screens = [intro, greeting, rules, game1, game2, game3];
+  showScreen(screen(state, () => {
+    showNextScreen(screens[screens.indexOf(screen) + 1]);
+  }));
 };
 
-const nextScreen = (currentScreen) => {
-  const next = getNextScreenNumber(currentScreen);
-  console.log(`Current: ${currentScreen}, Next: ${next}`);
-  console.log(`Name of Screen: ${SCREENS[currentScreen].name}`);
-  console.log(`Callback: `);
-  console.log(SCREENS[next].callback);
-  showScreen(SCREENS[currentScreen].name(state, SCREENS[next].callback));
-};
-
-nextScreen(0);
+showNextScreen(intro);
